087b34582a368e41ea9099a3cd3ef31d13f5859c
dimakrat <dimakrat@gmail.com>
2014-06-13 16:43:26 -0400

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## MakeCacheMatrix returns list of for functions
+## function set() - write original matrix x and NULL to variable m to scope of the list 
+## function get() - return original matrix x to the call scope
+## function setsolve() - write inverted matrix to m to scope of the list
+## function getsolve() - return inverted matrix m from scope of the list
+## to the call scope.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+MakeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve - invert matrix or read result from cach.
+## If x$getsolve() return NULL, caculate inverce of matrix stored in list x 
+## or read it from cache 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+CacheSolve <- function(x, ...) {
+        m <- x$getsolve()
+    if(!is.null(m)) {
+        message("cached data....")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+## I used samples in iscussion forums.
